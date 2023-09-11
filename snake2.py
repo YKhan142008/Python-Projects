1aa9e4e43176e973133f6a676e4b26e162c3cb48
YKhan142008 <117271479+YKhan142008@users.noreply.github.com>
2023-09-11 14:21:34 +0100

    Update snake2.py
    Fixed control bugs, Edited reset function and high_score

--- snake2.py
+++ snake2.py
@@ -20,13 +20,17 @@ color_value = [100,255]
 
 SNAKE_COLOR = [0,0,0]
 SNAKE_OUTLINE = [0,0,0]
-
-SNAKE_COLOR[random.randrange(0,3)] = color_value[rand_color_index]
-SNAKE_OUTLINE[random.randrange(0,3)] = color_value[rand_outline_index]
 BG_COLOR = (0,0,0)
+
+
 
 class Snake:
     def __init__(self,x,y,color,outline):
+        
+        SNAKE_COLOR[random.randrange(0,3)] = color_value[rand_color_index]
+        SNAKE_OUTLINE[random.randrange(0,3)] = color_value[rand_outline_index]
+        
+
         self.size = 20
         self.color = color
         self.x = x
@@ -43,34 +47,15 @@ class Snake:
         
         self.fruit = [random.randrange(0,w//self.size)*self.size,random.randrange(0,h//self.size)*self.size]
         
-    def high_score(self):
-        self.scores.append(self.score)
-        record = max(self.scores)
-        return record
-
-    
-    def reset(self,x,y,color,outline):
-
-        SNAKE_COLOR[random.randrange(0,3)] = color_value[rand_color_index]
-        SNAKE_OUTLINE[random.randrange(0,3)] = color_value[rand_outline_index]
-
-        self.size = 20
-        self.color = color
-        self.x = x
-        self.y = y
-        self.outline = outline
-  
-        
-        self.head = [x,y]
-        self.body = [self.head,[self.x-self.size,self.y],[self.x-self.size*2,self.y]]
-        self.direction = 'right'
-        self.score = 0
-
-        self.fruit = [random.randrange(0,w//self.size)*self.size,random.randrange(0,h//self.size)*self.size]
+    def reset(self):
+        screen.fill(BG_COLOR)
+        self.__init__(self.x,self.y,self.color,self.outline)
         
 
-        
-    
+    def render(self):
+        screen.fill(BG_COLOR)
+        self.draw_snake()
+        self.draw_fruit()
     
     
     def draw_snake(self):
@@ -105,15 +90,14 @@ class Snake:
 
         pygame.draw.rect(screen,'red',pygame.Rect(self.fruit[0],self.fruit[1],self.size,self.size))
     
-    def blit_score(self,s):
+    def blit_score(self,score,high_score):
         scores = []
-        scores.append(s)
-        font = pygame.font.Font('Arimo-Regular.ttf',25)
-        score = font.render(f'Score: {s} High Score: {self.high_score()}',True,'white')
+        scores.append(score)
+        font = pygame.font.Font('Arimo-Regular.ttf',10)
+        score = font.render(f'Score: {score} High Score: {high_score}',True,'white')
         screen.blit(score,[0,0])
     
-    
-    
+
     def game_over(self):
         x = self.head[0]
         y = self.head[1]
@@ -125,9 +109,8 @@ class Snake:
             return True
 
 
-
-
 def play():
+    high_score = 0
     x = w / 2
     y = h / 2
     snake = Snake(x,y,SNAKE_COLOR,SNAKE_OUTLINE)
@@ -138,38 +121,37 @@ def play():
                 pygame.quit()
                 exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT and snake.direction != 'left':
+                if (event.key == pygame.K_RIGHT or event.key == pygame.K_d) and snake.direction != 'left':
                     snake.direction = 'right'
-                if event.key == pygame.K_LEFT and snake.direction != 'right':
+                if (event.key == pygame.K_LEFT or event.key == pygame.K_a) and snake.direction != 'right':
                     snake.direction = 'left'
-                if event.key == pygame.K_DOWN and snake.direction != 'up':
+                if (event.key == pygame.K_DOWN or event.key == pygame.K_s) and snake.direction != 'up':
                     snake.direction = 'down'
-                if event.key == pygame.K_UP and snake.direction != 'down':
+                if (event.key == pygame.K_UP or event.key == pygame.K_w) and snake.direction != 'down':
                     snake.direction = 'up'
         
         
         
         if snake.game_over():
-            time.sleep(0.5)
-            snake.reset(x,y,SNAKE_COLOR,SNAKE_OUTLINE)
+            time.sleep(1)
+            snake.reset()
     
-        screen.fill(BG_COLOR)
-        snake.draw_snake()
-        snake.draw_fruit()
+        snake.render()
         snake.move_snake()
         
-
         snake.body.insert(0,snake.head)
 
         if snake.head == snake.fruit:
             snake.draw_fruit()
             snake.score += 1
+            if snake.score > high_score:
+                high_score += 1
         else:
             snake.body.pop()
       
-        snake.blit_score(snake.score)
+        snake.blit_score(snake.score,high_score)
         pygame.display.update()
-        clock.tick(10)
+        clock.tick(8)
 
 play()
 
