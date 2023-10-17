bfc557880d8dfbca5bb70c9299b8421426d9e1a8
YKhan142008 <117271479+YKhan142008@users.noreply.github.com>
2023-10-17 07:46:38 +0100

    Create constants.py
    constants used in the tictactoe file

---
+++ constants.py
@@ -0,0 +1,16 @@
+
+width,height = 400,400
+
+#grid attributes
+ROWS,COLS = 3,3
+grid_width = 4
+SQSIZE = width // COLS
+line_width = 7
+radius = SQSIZE // 2 - 15
+circ_width = 15
+
+#colors
+bg_color = '#31BDAC'
+grid_color = '#0DA18F'
+cross_color = '#545454'
+circle_color = '#F2EBD3'
